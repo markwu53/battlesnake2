ecfc69a25d4088935dc2235f534e877c6b44cd70
Mark Wu <mang.wu@gmail.com>
2026-01-16 17:53:52 -0500

    move chasing kill oppo to below split choice

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-a = []
-cond = all([x == 0 for x in a])
-print(cond)
+a = [i for i in range(49)]
+a = [i*i for i in a]
+a = [i % 49 for i in a]
+print(a)
+a = sorted(list(set(a)))
+print(a)
+a = [i%7 for i in a]
+print(a)
+
+# a = [i for i in range(7*7*7)]
+# a = [i*i for i in a]
+# a = [i % (7*7*7) for i in a]
+# a = sorted(list(set(a)))
+# a = [i%7 for i in a]
+# print(a)
+
+
