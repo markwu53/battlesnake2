fe61a7ae4c2f805e2d26e1327a294b1d2195765e
Mark Wu <mang.wu@gmail.com>
2026-02-24 16:28:14 -0500

    try split

--- try_split/decision_flow.py
+++ try_split/decision_flow.py
@@ -48,9 +48,9 @@ class GameTurn:
         self.turn = None
         self.vulnerables = []
 
-g = GameTurn()
-
 def main(game_state, log=True, log_db=False):
+
+    g = GameTurn()
 
     def ________DECISION_LOGIC________():
         return
