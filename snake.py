6e01d06f3e2ce617069a33c396dff763aff12de0
Mark Wu <mang.wu@gmail.com>
2026-04-13 06:56:47 -0400

    try meander when 1v1 gemini

--- snake.py
+++ snake.py
@@ -2,8 +2,8 @@ import typing
 #import decision_flow_one.decision_flow as local_main
 #import split.decision_flow as local_main
 #import territory.territory as local_main
-import territory2.territory as local_main
-# import territory_gemini.territory as local_main
+# import territory2.territory as local_main
+import territory_gemini.territory as local_main
 
 # info is called when you create your Battlesnake on play.battlesnake.com
 # and controls your Battlesnake's appearance
